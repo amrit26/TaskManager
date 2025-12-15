c870f1bb6e1c2a058d1a1e30e41f7f7b688e3636
amrit26 <amritheer@live.co.uk>
2025-12-15 18:10:41 +0000

    Validation put in

---
+++ Validation/CreateTaskRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using TaskManager.Api.Dtos;
+
+namespace TaskManager.Api.Validation;
+
+public sealed class CreateTaskRequestValidator : AbstractValidator<CreateTaskRequest>
+{
+    public CreateTaskRequestValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty()
+            .MaximumLength(200);
+        RuleFor(x => x.Description)
+            .MaximumLength(2000);
+        RuleFor(x => x.DueAt)
+            .Must(d => d > DateTimeOffset.UtcNow)
+            .WithMessage("DueAt must be in the future.");
+    }
+}
---
+++ Validation/UpdateTaskStatusRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using TaskManager.Api.Dtos;
+
+namespace TaskManager.Api.Validation;
+
+public sealed class UpdateTaskStatusRequestValidator : AbstractValidator<UpdateTaskStatusRequest>
+{
+    public UpdateTaskStatusRequestValidator()
+    {
+        RuleFor(x => x.Status).IsInEnum();
+    }
+}
