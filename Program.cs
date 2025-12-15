2ccd2a3b5113f0f397d0276818154b604fc10b67
amrit26 <amritheer@live.co.uk>
2025-12-15 18:31:23 +0000

    Global error handling and ConnectionStrings added.

--- Program.cs
+++ Program.cs
@@ -1,23 +1,47 @@
+using FluentValidation;
+using Hellang.Middleware.ProblemDetails;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Api.Data;
+using TaskManager.Api.Service;
+using TaskManager.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// EF Core + SQLite
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseSqlite(builder.Configuration.GetConnectionString("Sqlite")));
+
+// DI: repositories + services
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+builder.Services.AddScoped<ITaskService, TaskService>();
+
+// FluentValidation
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+
+// ProblemDetails (consistent API errors for frontend)
+builder.Services.AddProblemDetails(options =>
+{
+    options.IncludeExceptionDetails = (ctx, ex) => builder.Environment.IsDevelopment();
+
+    options.MapToStatusCode<ArgumentException>(StatusCodes.Status400BadRequest);
+    options.MapToStatusCode<InvalidOperationException>(StatusCodes.Status409Conflict);
+    options.MapToStatusCode<Exception>(StatusCodes.Status500InternalServerError);
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseProblemDetails();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseAuthorization();
-
+app.UseHttpsRedirection();
 app.MapControllers();
-
 app.Run();
